c90fe9d2e1804c67ace1701a695b11520f26af07
ShashankMangal <shashankmangal10@gmail.com>
2022-05-28 20:14:06 +0530

    call layout

--- app/src/main/java/com/sharkBytesLab/oneoone/MainActivity.java
+++ app/src/main/java/com/sharkBytesLab/oneoone/MainActivity.java
@@ -2,7 +2,11 @@ package com.sharkBytesLab.oneoone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.sharkBytesLab.oneoone.Screens.ConnectingActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.findbutton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(MainActivity.this, ConnectingActivity.class));
+                finish();
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/sharkBytesLab/oneoone/Screens/LoginActivity.java
+++ app/src/main/java/com/sharkBytesLab/oneoone/Screens/LoginActivity.java
@@ -2,15 +2,31 @@ package com.sharkBytesLab.oneoone.Screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.sharkBytesLab.oneoone.MainActivity;
 import com.sharkBytesLab.oneoone.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        findViewById(R.id.login_Button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                finish();
+
+            }
+        });
+
     }
 }
