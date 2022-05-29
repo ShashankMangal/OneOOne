3eb86375a2bbc3e33022a343d181e892b7544dc8
ShashankMangal <shashankmangal10@gmail.com>
2022-05-29 13:14:56 +0530

    login ready

--- app/src/main/java/com/sharkBytesLab/oneoone/Screens/LoginActivity.java
+++ app/src/main/java/com/sharkBytesLab/oneoone/Screens/LoginActivity.java
@@ -1,32 +1,113 @@
 package com.sharkBytesLab.oneoone.Screens;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.FirebaseDatabase;
 import com.sharkBytesLab.oneoone.MainActivity;
+import com.sharkBytesLab.oneoone.Models.User;
 import com.sharkBytesLab.oneoone.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-
+    private GoogleSignInClient mGoogleSignInClient;
+    private int RC_SIGN_IN = 11;
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                        .requestEmail().build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
         findViewById(R.id.login_Button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                finish();
+                Intent intent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(intent, RC_SIGN_IN);
+//                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+//                finish();
 
             }
         });
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RC_SIGN_IN)
+        {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            GoogleSignInAccount account = task.getResult();
+            authWithGoogle(account.getIdToken());
+        }
+    }
+
+    void authWithGoogle(String idToken)
+    {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful())
+                {
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    User firebaseUser = new User(user.getUid(), user.getDisplayName(), user.getPhotoUrl().toString(), "City", 50);
+                    database.getReference().child("Profiles").child(user.getUid()).setValue(firebaseUser).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            if(task.isSuccessful())
+                            {
+                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                finishAffinity();
+                            }
+                            else
+                            {
+                                Toast.makeText(LoginActivity.this, "Error : " + task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+                    Log.e("Profile",user.getPhotoUrl().toString());
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this, "Error : " + task.getException(), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+
 }
